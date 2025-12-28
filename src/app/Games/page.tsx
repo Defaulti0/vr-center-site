92588107818546dce420a669e9d43821e942c98f
Defaultio <crismartin994@gmail.com>
2025-12-27 23:01:38 -0500

    feat: Introduce new About, Events, Games, and Golf pages, along with a main landing page.

--- src/app/Games/page.tsx
+++ src/app/Games/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">About Page</h1>
-      <p className="text-2xl">The best About Page in the world</p>
+      <h1 className="text-4xl font-bold">Gemes Page</h1>
+      <p className="text-2xl">The best Games Page in the world</p>
       <button><a href="/">Home</a></button>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold">VR Center</h1>
       <p className="text-2xl">The best VR Center in the world</p>
+      <button><a href="/About">About</a></button>
+      <button><a href="/Events">Events</a></button>
+      <button><a href="/Games">Games</a></button>
+      <button><a href="/Golf">Golf</a></button>
     </div>
   );
 }
