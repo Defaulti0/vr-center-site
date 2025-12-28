50436de77c86a800987e9884f9c055059bcf26a4
Defaulti0 <crismartin994@gmail.com>
2025-12-27 22:02:47 -0500

    feat: initialize basic Next.js application with homepage, about page, and root layout

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">VR Center</h1>
-      <p className="text-2xl">The best VR Center in the world</p>
+      <h1 className="text-4xl font-bold">About Page</h1>
+      <p className="text-2xl">The best About Page in the world</p>
     </div>
   );
 }
