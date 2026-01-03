ca8ec334ecb0ecea78ccf4663e6cf41b7c6e3ee7
Defaultio <crismartin994@gmail.com>
2026-01-02 23:17:22 -0500

    feat: Add root layout with Geist fonts and a new Navbar component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 
-import Header from "./Components/header"
+// import Header from "./Components/header"
 import Footer from "./Components/footer"
+import Navbar from "./Components/navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
+        {/* <Header /> */}
+        <Navbar />
         {children}
         <Footer />
       </body>
